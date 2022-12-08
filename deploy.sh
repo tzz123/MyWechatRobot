5eaf27bcdce2dd778d04e8e88d2ef6b529f8c2cb
taacc <765373201@qq.com>
2022-12-08 17:35:36 +0800

    chore: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,11 @@ npm run build
 # navigate into the build output directory
 cd lib
 mv bundle.esm.js index.js
-cd -
+
 git init
 git add -A
 git commit -m 'deploy'
-git push
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+git push -f https://github.com/tzz123/MyWechatRobot.git master
+
+cd -
