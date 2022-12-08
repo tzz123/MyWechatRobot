af66425fd6d4af7dca6a009b25c185ebbaf03a3f
taacc <765373201@qq.com>
2022-12-08 17:29:54 +0800

    Revert "feat: update gitignore"
    This reverts commit 26c751e1159205ce4e5a5ba7d234f80c2d58e906.

--- deploy.sh
+++ deploy.sh
@@ -8,17 +8,11 @@ npm run build
 
 # navigate into the build output directory
 cd lib
-
 mv bundle.esm.js index.js
-
 cd -
 git init
 git add -A
 git commit -m 'deploy'
 git push
-
-git checkout master
-git checkout origin/develop/lib/index.js
-
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
